1880be826ffa3d90c0da31cbac26e8520c57256c
Andre Smith <andre@suitefolio.com>
2016-11-29 05:37:32 -0500

    Created simple script that turns on and off the led light strip

---
+++ server.js
@@ -0,0 +1,24 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function(){
+  var anode = new five.Led.RGB({
+    pins: {
+      red: 6,
+      green: 5,
+      blue: 3
+    },
+    isAnode: true
+  });
+
+  this.wait(5000, function(){
+    anode.toggle();
+  });
+
+  this.wait(10000, function(){
+    anote.off();
+  });
+
+  anode.on();
+  anode.color("#FF0000");
+});
